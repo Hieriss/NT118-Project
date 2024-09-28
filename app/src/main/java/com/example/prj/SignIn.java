f45b286f713e67595a7d37378493d43a3dc93f9b
Hieriss <tranminhhieeus@gmail.com>
2024-09-29 02:34:32 +0700

    update firebase realtime database

--- app/src/main/java/com/example/prj/SignIn.java
+++ app/src/main/java/com/example/prj/SignIn.java
@@ -4,15 +4,93 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
 public class SignIn extends AppCompatActivity {
+
+    EditText signinUsername, signinPassord;
+    Button signinButton;
+    TextView switchtosignupText;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    public Boolean validateUsername(){
+        String val = signinUsername.getText().toString();
+        if (val.isEmpty()){
+            signinUsername.setError("Username can't be empty");
+            return false;
+        }
+        else {
+            signinUsername.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatePassword(){
+        String val = signinPassord.getText().toString();
+        if (val.isEmpty()){
+            signinPassord.setError("Username can't be empty");
+            return false;
+        }
+        else {
+            signinPassord.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser(){
+        String userUsername = signinUsername.getText().toString().trim();
+        String userPassword = signinPassord.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("user");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    signinUsername.setError(null);
+                    String passwordFromDB = snapshot.child(userUsername).child("password").getValue(String.class);
+
+                    if (!Objects.equals(passwordFromDB, userPassword)){
+                        signinUsername.setError(null);
+                        Intent intent = new Intent(SignIn.this, MainPage.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        signinPassord.setError("Invalid Credentials");
+                        signinPassord.requestFocus();
+                    }
+                }
+                else {
+                    signinUsername.setError("Username didn't exist");
+                    signinUsername.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +103,23 @@ public class SignIn extends AppCompatActivity {
             return insets;
         });
 
-        Button buttonsignin = findViewById(R.id.signin_button);
-        buttonsignin.setOnClickListener(new View.OnClickListener() {
+        signinUsername = findViewById(R.id.signin_username);
+        signinPassord = findViewById(R.id.signin_password);
+
+        signinButton = findViewById(R.id.signin_button);
+        signinButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), MainPage.class);
-                view.getContext().startActivity(intent);
+                if (!validateUsername() | !validatePassword()){
+                }
+                else {
+                    checkUser();
+                }
             }
         });
 
-        TextView switchtosignup = findViewById(R.id.signin_text3);
-        switchtosignup.setOnClickListener(new View.OnClickListener() {
+        switchtosignupText = findViewById(R.id.signin_text3);
+        switchtosignupText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), SignUp.class);
--- app/src/main/java/com/example/prj/SignUp.java
+++ app/src/main/java/com/example/prj/SignUp.java
@@ -2,17 +2,31 @@ package com.example.prj;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ThemedSpinnerAdapter;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignUp extends AppCompatActivity {
+
+    Button signupButton;
+    TextView switchtosigninText;
+    EditText signupUsername, signupPassword, signupConfirmpassword, signupEmail, signupPhone;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +39,57 @@ public class SignUp extends AppCompatActivity {
             return insets;
         });
 
-        Button buttonsignup = findViewById(R.id.signup_button);
-        buttonsignup.setOnClickListener(new View.OnClickListener() {
+        signupUsername = findViewById(R.id.signup_username);
+        signupPassword = findViewById(R.id.signup_password);
+        signupConfirmpassword = findViewById(R.id.signup_confirmpassword);
+        signupEmail = findViewById(R.id.signup_email);
+        signupPhone = findViewById(R.id.signup_phone);
+
+        signupButton = findViewById(R.id.signup_button);
+        signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), SignIn.class);
-                view.getContext().startActivity(intent);
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("user");
+
+                String username = signupUsername.getText().toString();
+                String password = signupPassword.getText().toString();
+                String confirmpassword = signupConfirmpassword.getText().toString();
+                String email = signupEmail.getText().toString();
+                String phone = signupPhone.getText().toString();
+
+                if(TextUtils.isEmpty(username)){
+                    Toast.makeText(SignUp.this, "Username can't be empty", Toast.LENGTH_SHORT).show();
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(SignUp.this, "Password can't be empty", Toast.LENGTH_SHORT).show();
+                }
+                if(TextUtils.isEmpty(confirmpassword)){
+                    Toast.makeText(SignUp.this, "Confirm password can't be empty", Toast.LENGTH_SHORT).show();
+                }
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(SignUp.this, "Email can't be empty", Toast.LENGTH_SHORT).show();
+                }
+                if(TextUtils.isEmpty(phone)){
+                    Toast.makeText(SignUp.this, "Phone Number can't be empty", Toast.LENGTH_SHORT).show();
+                }
+
+                if (password.equals(confirmpassword)){
+                    HelperClass helperClass = new HelperClass(username, password, email, phone);
+                    reference.child(username).setValue(helperClass);
+
+                    Toast.makeText(SignUp.this, "Signed up successfully!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(view.getContext(), SignIn.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(SignUp.this, "Password didn't match", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
-        TextView switchtosignin = findViewById(R.id.signup_text3);
-        switchtosignin.setOnClickListener(new View.OnClickListener() {
+        switchtosigninText = findViewById(R.id.signup_text3);
+        switchtosigninText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), SignIn.class);
